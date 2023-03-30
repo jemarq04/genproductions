03541b3d1abb747ae04f9f80b3b8aed169a8969f
Justin Marquez <justin.marquez@wisc.edu>
2023-03-30 11:55:37 -0500

    preparing directories for pp_eemm-cll1_massless and ZTZT_LO generation

---
+++ bin/MadGraph5_aMCatNLO/out/ZTZT_LO/generate_ZTZT_LO.sh
@@ -0,0 +1,1 @@
+../../gridpack_generation.sh ZTZT_LO cards/ local ALL $SCRAM_ARCH CMSSW_12_3_0
---
+++ bin/MadGraph5_aMCatNLO/out/pp_eemm-cll1_massless/generate_pp_eemm-cll1_massless.sh
@@ -0,0 +1,1 @@
+../../gridpack_generation.sh pp_eemm-cll1_massless cards local ALL $SCRAM_ARCH CMSSW_12_3_0
